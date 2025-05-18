0f2dcc1228ccb93486cac3e01f4e98d82ce1214b
ross96D <abelaross96@gmail.com>
2025-05-18 16:19:13 -0400

    install appimagetool if not found

--- linux/create-appimage.sh
+++ linux/create-appimage.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+if ! type appimagetool > /dev/null; then 
+    ARCH="$(uname -m)"
+    wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-"$ARCH".AppImage
+    chmod +x appimagetool-"$ARCH".AppImage
+    if ls "$HOME"/.local/bin > /dev/null; then
+        mv appimagetool-"$ARCH".AppImage "$HOME"/.local/bin
+    else
+        mv appimagetool-"$ARCH".AppImage /usr/bin
+    fi 
+fi
+
 rm -rf AppDir
 cp -r build/linux/x64/release/bundle AppDir
 echo "
