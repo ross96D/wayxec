bffb0c033c8bb5748e8f74e89a5b6c74de9f9e3a
ross96D <abelaross96@gmail.com>
2025-05-18 17:09:21 -0400

    statically link librust_icon_lookup

--- linux/create-appimage.sh
+++ linux/create-appimage.sh
@@ -2,12 +2,15 @@
 APPIMAGETOOL=""
 if ! type appimagetool > /dev/null; then 
     ARCH="$(uname -m)"
-    wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-"$ARCH".AppImage
+    # shellcheck disable=SC2210
+    wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-"$ARCH".AppImage > /dev/null 2>1
     chmod +x appimagetool-"$ARCH".AppImage
     APPIMAGETOOL="$PWD/appimagetool-$ARCH.AppImage"
 else
     APPIMAGETOOL=$(which appimagetool)
 fi
+
+echo "appimagetoo located in $APPIMAGETOOL"
 
 rm -rf AppDir
 cp -r build/linux/x64/release/bundle AppDir
