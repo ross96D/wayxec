60e16a2e6ab2c7514a93f5e542516e662d5da914
ross96D <abelaross96@gmail.com>
2025-06-06 18:29:32 -0400

    vendor wayland_layer_shell

---
+++ packages/wayland_layer_shell/lib/wayland_layer_shell.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/services.dart';
+import 'package:wayland_layer_shell/types.dart';
+
+class WaylandLayerShell {
+  final methodChannel = const MethodChannel('wayland_layer_shell');
+
+  Future<String?> getPlatformVersion() async {
+    final version =
+        await methodChannel.invokeMethod<String>('getPlatformVersion');
+    return version;
+  }
+
+  /// Returns: 'true' if the platform is Wayland and Wayland compositor supports the
+  /// zwlr_layer_shell_v1 protocol. If not supported, returns 'false' and initialize
+  /// gtk window as normal window
+  Future<bool> isLayerShellSupported() async {
+    return await methodChannel.invokeMethod('isSupported');
+  }
+
+  /// @width: The width of the surface. default is 1280
+  /// @height: The height of the surface. default is 720
+  ///
+  /// Initialize the layer shell protocol.
+  /// checks if the platform is Wayland and Wayland compositor supports the
+  /// zwlr_layer_shell_v1 protocol so no need use isLayerShellSupported first.
+  ///
+  /// Returns: 'true' if platform is Wayland and Wayland compositor supports
+  /// the zwlr_layer_shell_v1 protocol, if not supported, returns 'false' and initialize
+  /// gtk window as normal window
+  Future<bool> initialize([int? width, int? height]) async {
+    final Map<String, dynamic> arguments = {
+      'width': width ?? 1280,
+      'height': height ?? 720,
+    };
+    return await methodChannel.invokeMethod('initialize', arguments);
+  }
+
+  /// @layer: The [ShellLayer] on which this surface appears.
+  ///
+  /// Set the "layer" on which the surface appears (controls if it is over top of or below other surfaces). The layer may
+  /// be changed on-the-fly in the current version of the layer shell protocol, but on compositors that only support an
+  /// older version the @window is remapped so the change can take effect.
+  ///
+  /// Default is %GTK_LAYER_SHELL_LAYER_TOP
+  Future<void> setLayer(ShellLayer layer) async {
+    final Map<String, dynamic> arguments = {
+      'layer': layer.index,
+    };
+    await methodChannel.invokeMethod('setLayer', arguments);
+  }
+
+  /// Returns: the current layer as [ShellLayer].
+  Future<ShellLayer> getLayer() async {
+    return ShellLayer
+        .values[(await methodChannel.invokeMethod('getLayer')) as int];
+  }
+
+  /// Returns: the list of all [Monitor]s connected to the computer.
+  Future<List<Monitor>> getMonitorList() async {
+    List<String> monitors =
+        List<String>.from(await methodChannel.invokeMethod('getMonitorList'));
+    return monitors.map((e) {
+      final i = e.indexOf(':');
+      return Monitor(int.parse(e.substring(0, i)), e.substring(i + 1));
+    }).toList();
+  }
+
+  /// @monitor: The [Monitor] that this surface will be placed on.
+  /// (null to let the compositor decide)
+  ///
+  /// Set the monitor this surface will be placed on.
+  Future<void> setMonitor(Monitor? monitor) async {
+    final Map<String, dynamic> arguments = {
+      'id': monitor == null ? -1 : monitor.id,
+    };
+    await methodChannel.invokeMethod('setMonitor', arguments);
+  }
+
+  /// @edge: A [ShellEdge] this layer surface may be anchored to.
+  /// @anchor_to_edge: Whether or not to anchor this layer surface to @edge.
+  ///
+  /// Set whether this layer surface should be anchored to @edge.
+  /// - If two perpendicular edges are anchored, the surface with be anchored to that corner
+  /// - If two opposite edges are anchored, the window will be stretched across the screen in that direction
+  ///
+  /// Default is %FALSE for each [ShellEdge]
+  Future<void> setAnchor(ShellEdge edge, bool anchorToEdge) async {
+    final Map<String, dynamic> arguments = {
+      'edge': edge.index,
+      'anchor_to_edge': anchorToEdge
+    };
+    await methodChannel.invokeMethod('setAnchor', arguments);
+  }
+
+  /// @edge: A [ShellEdge] this layer surface may be anchored to.
+  ///
+  /// Returns: if this surface is anchored to the given edge.
+  Future<bool> getAnchor(ShellEdge edge) async {
+    final Map<String, dynamic> arguments = {
+      'edge': edge.index,
+    };
+    return await methodChannel.invokeMethod('getAnchor', arguments);
+  }
+
+  /// @edge: The [ShellEdge] for which to set the margin.
+  /// @marginSize: The margin for @edge to be set.
+  ///
+  /// Set the margin for a specific @edge of a @window. Effects both surface's distance from
+  /// the edge and its exclusive zone size (if auto exclusive zone enabled).
+  ///
+  /// Default is 0 for each [ShellEdge]
+  Future<void> setMargin(ShellEdge edge, int marginSize) async {
+    final Map<String, dynamic> arguments = {
+      'edge': edge.index,
+      'margin_size': marginSize
+    };
+    await methodChannel.invokeMethod('setMargin', arguments);
+  }
+
+  /// @edge: The [ShellEdge] for which to get the margin.
+  ///
+  /// Returns: the size of the margin for the given edge.
+  Future<int> getMargin(ShellEdge edge) async {
+    final Map<String, dynamic> arguments = {
+      'edge': edge.index,
+    };
+    return await methodChannel.invokeMethod('getMargin', arguments);
+  }
+
+  /// @exclusiveZone: The size of the exclusive zone.
+  ///
+  /// Set the size of the exclusive zone.
+  ///
+  /// Has no effect unless the surface is anchored to an edge. Requests that the compositor
+  /// does not place other surfaces within the given exclusive zone of the anchored edge.
+  /// For example, a panel can request to not be covered by maximized windows. See
+  /// wlr-layer-shell-unstable-v1.xml for details.
+  ///
+  /// Default is 0
+  Future<void> setExclusiveZone(int exclusiveZone) async {
+    final Map<String, dynamic> arguments = {'exclusive_zone': exclusiveZone};
+    await methodChannel.invokeMethod('setExclusiveZone', arguments);
+  }
+
+  /// Returns: the window's exclusive zone (which may have been set manually or automatically)
+  Future<int> getExclusiveZone() async {
+    return await methodChannel.invokeMethod('getExclusiveZone');
+  }
+
+  /// Enable auto exclusive zone
+  ///
+  /// When auto exclusive zone is enabled, exclusive zone is automatically set to the
+  /// size of the @window + relevant margin. To disable auto exclusive zone, just set the
+  /// exclusive zone to 0 or any other fixed value.
+  Future<void> enableAutoExclusiveZone() async {
+    await methodChannel.invokeMethod('enableAutoExclusiveZone');
+  }
+
+  /// Returns: if the surface's exclusive zone is set to change based on the window's size
+  Future<bool> isAutoExclusiveZoneEnabled() async {
+    return await methodChannel.invokeMethod('isAutoExclusiveZoneEnabled');
+  }
+
+  /// @mode: The type of keyboard interactivity requested.
+  ///
+  /// Sets if/when @window should receive keyboard events from the compositor, see
+  /// [ShellKeyboardMode] for details.
+  ///
+  /// Default is [ShellKeyboardMode.keyboardModeNone]
+  Future<void> setKeyboardMode(ShellKeyboardMode mode) async {
+    final Map<String, dynamic> arguments = {'keyboard_mode': mode.index};
+    await methodChannel.invokeMethod('setKeyboardMode', arguments);
+  }
+
+  /// Returns: current keyboard interactivity mode for window
+  Future<ShellKeyboardMode> getKeyboardMode() async {
+    return ShellKeyboardMode
+        .values[(await methodChannel.invokeMethod('getKeyboardMode')) as int];
+  }
+}
---
+++ packages/wayland_layer_shell/linux/wayland_layer_shell_plugin.cc
@@ -0,0 +1,321 @@
+#include "include/wayland_layer_shell/wayland_layer_shell_plugin.h"
+
+#include <flutter_linux/flutter_linux.h>
+#include <gtk/gtk.h>
+#include <sys/utsname.h>
+
+#include <cstring>
+
+#include "wayland_layer_shell_plugin_private.h"
+
+#include <gtk-layer-shell/gtk-layer-shell.h>
+#include "iostream"
+
+#define WAYLAND_LAYER_SHELL_PLUGIN(obj)                                     \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj), wayland_layer_shell_plugin_get_type(), \
+                              WaylandLayerShellPlugin))
+
+struct _WaylandLayerShellPlugin
+{
+  GObject parent_instance;
+  FlPluginRegistrar *registrar;
+};
+
+G_DEFINE_TYPE(WaylandLayerShellPlugin, wayland_layer_shell_plugin, g_object_get_type())
+
+GtkWindow *get_window(WaylandLayerShellPlugin *self)
+{
+  FlView *view = fl_plugin_registrar_get_view(self->registrar);
+  if (view == nullptr)
+    return nullptr;
+
+  return GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(view)));
+}
+
+static FlMethodResponse *is_supported(WaylandLayerShellPlugin *self)
+{
+  if (gtk_layer_is_supported() == 0)
+  {
+    GtkWindow *gtk_window = get_window(self);
+    gtk_widget_show(GTK_WIDGET(gtk_window));
+  }
+  g_autoptr(FlValue) result =
+      fl_value_new_bool(gtk_layer_is_supported());
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+}
+
+static FlMethodResponse *initialize(WaylandLayerShellPlugin *self, FlValue *args)
+{
+  g_autoptr(FlValue) result;
+  GtkWindow *gtk_window = get_window(self);
+  if (gtk_layer_is_supported() == 0)
+  {
+    result = fl_value_new_bool(false);
+  }
+  else
+  {
+    int width = fl_value_get_int(fl_value_lookup_string(args, "width"));
+    int height = fl_value_get_int(fl_value_lookup_string(args, "height"));
+    gtk_widget_set_size_request(GTK_WIDGET(gtk_window), width, height);
+    gtk_layer_init_for_window(gtk_window);
+    result = fl_value_new_bool(true);
+  }
+
+  gtk_widget_show(GTK_WIDGET(gtk_window));
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+}
+
+static FlMethodResponse *set_layer(WaylandLayerShellPlugin *self, FlValue *args)
+{
+  int layer = fl_value_get_int(fl_value_lookup_string(args, "layer"));
+  gtk_layer_set_layer(get_window(self), static_cast<GtkLayerShellLayer>(layer));
+  g_autoptr(FlValue) result = fl_value_new_bool(true);
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+}
+
+static FlMethodResponse *get_layer(WaylandLayerShellPlugin *self)
+{
+  g_autoptr(FlValue) result = fl_value_new_int(gtk_layer_get_layer(get_window(self)));
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+}
+
+static FlMethodResponse *get_monitor_list(WaylandLayerShellPlugin *self)
+{
+  GdkDisplay *display = gdk_display_get_default();
+  g_autoptr(FlValue) result = fl_value_new_list();
+  for (int i = 0; i < gdk_display_get_n_monitors(display); i++)
+  {
+    GdkMonitor *monitor = gdk_display_get_monitor(display, i);
+    gchar *val = g_strdup_printf("%i:%s", i, gdk_monitor_get_model(monitor));
+    fl_value_append_take(result, fl_value_new_string(val));
+  }
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+}
+
+static FlMethodResponse *set_monitor(WaylandLayerShellPlugin *self, FlValue *args)
+{
+  GdkDisplay *display = gdk_display_get_default();
+  int id = fl_value_get_int(fl_value_lookup_string(args, "id"));
+
+  if (id == -1)
+  {
+    gtk_layer_set_monitor(get_window(self), NULL);
+
+    g_autoptr(FlValue) result = fl_value_new_bool(true);
+    return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+  }
+
+  GdkMonitor *monitor = gdk_display_get_monitor(display, id);
+  gtk_layer_set_monitor(get_window(self), monitor);
+
+  g_autoptr(FlValue) result = fl_value_new_bool(true);
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+}
+
+static FlMethodResponse *set_anchor(WaylandLayerShellPlugin *self, FlValue *args)
+{
+  int edge = fl_value_get_int(fl_value_lookup_string(args, "edge"));
+  gboolean anchor_to_edge = fl_value_get_bool(fl_value_lookup_string(args, "anchor_to_edge"));
+
+  gtk_layer_set_anchor(get_window(self), static_cast<GtkLayerShellEdge>(edge), anchor_to_edge);
+  g_autoptr(FlValue) result = fl_value_new_bool(true);
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+}
+
+static FlMethodResponse *get_anchor(WaylandLayerShellPlugin *self, FlValue *args)
+{
+  int edge = fl_value_get_int(fl_value_lookup_string(args, "edge"));
+  g_autoptr(FlValue) result = fl_value_new_bool(gtk_layer_get_anchor(get_window(self), static_cast<GtkLayerShellEdge>(edge)));
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+}
+
+static FlMethodResponse *set_margin(WaylandLayerShellPlugin *self, FlValue *args)
+{
+  int edge = fl_value_get_int(fl_value_lookup_string(args, "edge"));
+  int margin_size = fl_value_get_int(fl_value_lookup_string(args, "margin_size"));
+
+  gtk_layer_set_margin(get_window(self), static_cast<GtkLayerShellEdge>(edge), margin_size);
+  g_autoptr(FlValue) result = fl_value_new_bool(true);
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+}
+
+static FlMethodResponse *get_margin(WaylandLayerShellPlugin *self, FlValue *args)
+{
+  int edge = fl_value_get_int(fl_value_lookup_string(args, "edge"));
+  g_autoptr(FlValue) result = fl_value_new_int(gtk_layer_get_margin(get_window(self), static_cast<GtkLayerShellEdge>(edge)));
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+}
+
+static FlMethodResponse *set_exclusive_zone(WaylandLayerShellPlugin *self, FlValue *args)
+{
+  int exclusive_zone = fl_value_get_int(fl_value_lookup_string(args, "exclusive_zone"));
+  gtk_layer_set_exclusive_zone(get_window(self), exclusive_zone);
+  g_autoptr(FlValue) result = fl_value_new_bool(true);
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+}
+
+static FlMethodResponse *get_exclusive_zone(WaylandLayerShellPlugin *self)
+{
+  g_autoptr(FlValue) result = fl_value_new_int(gtk_layer_get_exclusive_zone(get_window(self)));
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+}
+
+static FlMethodResponse *enable_auto_exclusive_zone(WaylandLayerShellPlugin *self)
+{
+  gtk_layer_auto_exclusive_zone_enable(get_window(self));
+  g_autoptr(FlValue) result = fl_value_new_bool(true);
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+}
+
+static FlMethodResponse *is_auto_exclusive_zone_enabled(WaylandLayerShellPlugin *self)
+{
+  g_autoptr(FlValue) result = fl_value_new_bool(gtk_layer_auto_exclusive_zone_is_enabled(get_window(self)));
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+}
+
+static FlMethodResponse *set_keyboard_mode(WaylandLayerShellPlugin *self, FlValue *args)
+{
+  int keyboard_mode = fl_value_get_int(fl_value_lookup_string(args, "keyboard_mode"));
+  gtk_layer_set_keyboard_mode(get_window(self), static_cast<GtkLayerShellKeyboardMode>(keyboard_mode));
+  g_autoptr(FlValue) result = fl_value_new_bool(true);
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+}
+
+static FlMethodResponse *get_keyboard_mode(WaylandLayerShellPlugin *self)
+{
+  g_autoptr(FlValue) result = fl_value_new_int(gtk_layer_get_keyboard_mode(get_window(self)));
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+}
+
+// Called when a method call is received from Flutter.
+static void wayland_layer_shell_plugin_handle_method_call(
+    WaylandLayerShellPlugin *self,
+    FlMethodCall *method_call)
+{
+  g_autoptr(FlMethodResponse) response = nullptr;
+
+  const gchar *method = fl_method_call_get_name(method_call);
+  FlValue *args = fl_method_call_get_args(method_call);
+
+  if (strcmp(method, "getPlatformVersion") == 0)
+  {
+    response = get_platform_version();
+  }
+  else if (strcmp(method, "isSupported") == 0)
+  {
+    response = is_supported(self);
+  }
+  else if (strcmp(method, "initialize") == 0)
+  {
+    response = initialize(self, args);
+  }
+  else if (strcmp(method, "setLayer") == 0)
+  {
+    response = set_layer(self, args);
+  }
+  else if (strcmp(method, "getLayer") == 0)
+  {
+    response = get_layer(self);
+  }
+  else if (strcmp(method, "getMonitorList") == 0)
+  {
+    response = get_monitor_list(self);
+  }
+  else if (strcmp(method, "setMonitor") == 0)
+  {
+    response = set_monitor(self, args);
+  }
+  else if (strcmp(method, "setAnchor") == 0)
+  {
+    response = set_anchor(self, args);
+  }
+  else if (strcmp(method, "getAnchor") == 0)
+  {
+    response = get_anchor(self, args);
+  }
+  else if (strcmp(method, "setMargin") == 0)
+  {
+    response = set_margin(self, args);
+  }
+  else if (strcmp(method, "getMargin") == 0)
+  {
+    response = get_margin(self, args);
+  }
+  else if (strcmp(method, "setExclusiveZone") == 0)
+  {
+    response = set_exclusive_zone(self, args);
+  }
+  else if (strcmp(method, "getExclusiveZone") == 0)
+  {
+    response = get_exclusive_zone(self);
+  }
+  else if (strcmp(method, "enableAutoExclusiveZone") == 0)
+  {
+    response = enable_auto_exclusive_zone(self);
+  }
+  else if (strcmp(method, "isAutoExclusiveZoneEnabled") == 0)
+  {
+    response = is_auto_exclusive_zone_enabled(self);
+  }
+  else if (strcmp(method, "setKeyboardMode") == 0)
+  {
+    response = set_keyboard_mode(self, args);
+  }
+  else if (strcmp(method, "getKeyboardMode") == 0)
+  {
+    response = get_keyboard_mode(self);
+  }
+  else
+  {
+    response = FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
+  }
+
+  fl_method_call_respond(method_call, response, nullptr);
+}
+
+FlMethodResponse *get_platform_version()
+{
+  struct utsname uname_data = {};
+  uname(&uname_data);
+  g_autofree gchar *version = g_strdup_printf("Linux %s", uname_data.version);
+  g_autoptr(FlValue) result = fl_value_new_string(version);
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+}
+
+static void wayland_layer_shell_plugin_dispose(GObject *object)
+{
+  G_OBJECT_CLASS(wayland_layer_shell_plugin_parent_class)->dispose(object);
+}
+
+static void wayland_layer_shell_plugin_class_init(WaylandLayerShellPluginClass *klass)
+{
+  G_OBJECT_CLASS(klass)->dispose = wayland_layer_shell_plugin_dispose;
+}
+
+static void wayland_layer_shell_plugin_init(WaylandLayerShellPlugin *self) {}
+
+static void method_call_cb(FlMethodChannel *channel, FlMethodCall *method_call,
+                           gpointer user_data)
+{
+  WaylandLayerShellPlugin *plugin = WAYLAND_LAYER_SHELL_PLUGIN(user_data);
+  wayland_layer_shell_plugin_handle_method_call(plugin, method_call);
+}
+
+void wayland_layer_shell_plugin_register_with_registrar(FlPluginRegistrar *registrar)
+{
+  WaylandLayerShellPlugin *plugin = WAYLAND_LAYER_SHELL_PLUGIN(
+      g_object_new(wayland_layer_shell_plugin_get_type(), nullptr));
+
+  plugin->registrar = FL_PLUGIN_REGISTRAR(g_object_ref(registrar));
+
+  g_autoptr(FlStandardMethodCodec) codec = fl_standard_method_codec_new();
+  g_autoptr(FlMethodChannel) channel =
+      fl_method_channel_new(fl_plugin_registrar_get_messenger(registrar),
+                            "wayland_layer_shell",
+                            FL_METHOD_CODEC(codec));
+  fl_method_channel_set_method_call_handler(channel, method_call_cb,
+                                            g_object_ref(plugin),
+                                            g_object_unref);
+
+  g_object_unref(plugin);
+}
