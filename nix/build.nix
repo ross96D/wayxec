6c7a3aba8aed2d4df3387cc48068167b4c53db4b
ross96D <abelaross96@gmail.com>
2025-06-16 17:37:49 -0400

    fix github build flutter version

--- nix/build.nix
+++ nix/build.nix
@@ -6,10 +6,10 @@
 # wrap the existing release appImage so it runs on nixOS
 appimageTools.wrapType2 rec {
   pname = "wayxec";
-  version = "1.0.1";
+  version = "1.0.4";
 
   src = fetchurl {
-    url = "https://github.com/ross96D/wayxec/releases/download/1.0.1/wayxec-x86_64.AppImage";
+    url = "https://github.com/ross96D/wayxec/releases/download/1.0.4/wayxec-x86_64.AppImage";
     hash = "sha256-vSqfhpNbnoH8B/CApd0xIvZlYeeBv8vtJ+sizalSsUg=";
   };
 
@@ -19,37 +19,3 @@ appimageTools.wrapType2 rec {
   ];
 
 }
-
-
-  # this is the method that we were originally using, which compiles
-  # from source. Hard to do because of rust deps that are downloaded
-  # at compile time, which nix doesn'r allow
-#{
-#  flutter319,
-#  fetchFromGitHub,
-#  pkgs
-#} :
-#flutter319.buildFlutterApp {
-#
-#  src = fetchFromGitHub {
-#    # https://github.com/ross96D/wayxec
-#    owner = "ross96D";
-#    repo = "wayxec";
-#    rev = "1.0.1";
-#    sha256 = "sha256-c5hjOi1F0fsNgGqE+bsQy7fsY0ZYXUz3buDc/U9GPSo=";
-#  };
-#
-#  buildInputs = with pkgs; [
-#    gtk-layer-shell
-#    corrosion
-#  ];
-#
-#  autoPubspecLock = src + "/pubspec.lock";
-#
-#  buildPhase = ''
-#    runHook preBuild
-#    mkdir -p build/flutter_assets/fonts
-#    flutter build linux apps/firmware_updater/lib/main.dart
-#    runHook postBuild
-#  '';
-#}
