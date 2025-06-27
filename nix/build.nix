610fa668c57bccbd696547f08e9e5b435dd578b0
David Alcalde <david.alcalde.cu@gmail.com>
2025-06-27 16:15:46 -0500

    Update nix build to version 1.0.5

--- nix/build.nix
+++ nix/build.nix
@@ -1,21 +1,17 @@
-{
-  appimageTools,
-  fetchurl,
-  pkgs
-} :
+{ appimageTools, fetchurl, pkgs }:
+
 # wrap the existing release appImage so it runs on nixOS
 appimageTools.wrapType2 rec {
+
   pname = "wayxec";
-  version = "1.0.4";
+  version = "1.0.5";
 
   src = fetchurl {
-    url = "https://github.com/ross96D/wayxec/releases/download/1.0.4/wayxec-x86_64.AppImage";
-    hash = "sha256-vSqfhpNbnoH8B/CApd0xIvZlYeeBv8vtJ+sizalSsUg=";
+    url =
+      "https://github.com/ross96D/wayxec/releases/download/1.0.5/wayxec-x86_64.AppImage";
+    hash = "sha256-E5po2aLsCVxN8nXgEUMzzkg5PNVQUL8g330950v8K1c=";
   };
 
-  extraPkgs = pkgs: [
-    pkgs.gtk-layer-shell
-    pkgs.libepoxy
-  ];
+  extraPkgs = pkgs: [ pkgs.gtk-layer-shell pkgs.libepoxy ];
 
 }
