4c8207af7c9f9b56b883b7387d8d4cb5e3cc989b
David Alcalde <david.alcalde.cu@gmail.com>
2025-07-08 21:06:29 -0500

    Fix hash

--- nix/build.nix
+++ nix/build.nix
@@ -9,7 +9,7 @@ appimageTools.wrapType2 rec {
   src = fetchurl {
     url =
       "https://github.com/ross96D/wayxec/releases/download/${version}/wayxec-x86_64.AppImage";
-    hash = "sha256-E5po2aLsCVxN8nXgEUMzzkg5PNVQUL8g330950v8K1c=";
+    hash = "sha256-UDutI4xxUMY8J2dSqIVP0i7tNiLXeGleSzWwB8fK6Ac=";
   };
 
   extraPkgs = pkgs: [ pkgs.gtk-layer-shell pkgs.libepoxy ];
