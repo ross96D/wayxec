e02b802313e6d286fb4c2ee86f9874dfeed45848
David Alcalde <david.alcalde.cu@gmail.com>
2025-07-13 01:38:02 -0500

    Fix build.nix

--- nix/build.nix
+++ nix/build.nix
@@ -6,17 +6,17 @@ appimageTools.wrapType2 {
   pname = "wayxec";
   version = "<version>";
 
-  src = ./wayxec-x86_64.AppImage;
+  src = "wayxec-x86_64.AppImage";
 
   extraPkgs = pkgs: [ pkgs.gtk-layer-shell pkgs.libepoxy ];
-    
-  extraInstallCommands = ''
-      # Install .desktop file
-      mkdir -p $out/share/applications
-      cp linux/wayxec.desktop $out/share/applications/wayxec.desktop
 
-      # Install icon
-      mkdir -p $out/share/icons/hicolor/256x256/apps
-      cp linux/wayxec.png $out/share/icons/hicolor/256x256/apps/wayxec.png
-    '';
+  extraInstallCommands = ''
+    # Install .desktop file
+    mkdir -p $out/share/applications
+    cp linux/wayxec.desktop $out/share/applications/wayxec.desktop
+
+    # Install icon
+    mkdir -p $out/share/icons/hicolor/256x256/apps
+    cp linux/wayxec.png $out/share/icons/hicolor/256x256/apps/wayxec.png
+  '';
 }
